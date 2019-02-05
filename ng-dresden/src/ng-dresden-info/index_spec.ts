a051bac69040b5b51b4506f8ece38a148dbfa9b3
Gregor Doroschenko <g.doroschenko@gmail.com>
2019-02-06 00:46:54 +0100

    rename ng-dresden to ng-dresen-info

--- ng-dresden/src/ng-dresden-info/index_spec.ts
+++ ng-dresden/src/ng-dresden-info/index_spec.ts
@@ -6,10 +6,10 @@ import * as path from 'path';
 const collectionPath = path.join(__dirname, '../collection.json');
 
 
-describe('ng-dresden', () => {
+describe('ng-dresden-info', () => {
   it('works', () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('ng-dresden', {}, Tree.empty());
+    const tree = runner.runSchematic('ng-dresden-info', {}, Tree.empty());
 
     expect(tree.files).toEqual([]);
   });
