ffa6daf988d4989d30e5250bea37cf7ffe83d228
Gregor Doroschenko <g.doroschenko@gmail.com>
2019-02-06 02:01:42 +0100

    create chain rule for ng-dresden-component schematic

--- ng-dresden/src/ng-dresden-component/index.ts
+++ ng-dresden/src/ng-dresden-component/index.ts
@@ -1,10 +1,36 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
+import {
+  apply,
+  branchAndMerge,
+  chain, mergeWith,
+  move,
+  Rule,
+  SchematicContext,
+  template,
+  Tree,
+  url
+} from '@angular-devkit/schematics';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function ngDresdenComponent(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+export function ngDresdenComponent(options: any): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+
+    const templateSource = apply(url('./files'), [
+      template({
+        ...strings,
+        ...options
+      }),
+      move(options.path || '')
+    ]);
+
+    const rule = chain([
+      branchAndMerge(chain([
+        mergeWith(templateSource)
+      ]))
+    ]);
+
+    return rule(tree, context);
   };
 }
